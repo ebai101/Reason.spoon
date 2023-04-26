766b8ae9ae3baed57f8858bbc56e93653a4f635d
Ethan Bailey <ebailey256@gmail.com>
2023-04-26 14:45:56 -0400

    formatted colorList

--- color_picker.lua
+++ color_picker.lua
@@ -7,134 +7,38 @@ local log = hs.logger.new('color', 'debug')
 -- * name: the color name, as shown in the menus
 -- * hex: the hex color representation of the track color
 colorPicker.colorList = {
-    {
-        name = 'Burgundy',
-        hex = '#9D1D21'
-    },
-    {
-        name = 'Red',
-        hex = '#D62127'
-    },
-    {
-        name = 'Orange',
-        hex = '#DE5F26'
-    },
-    {
-        name = 'Brown',
-        hex = '#854F20'
-    },
-    {
-        name = 'Ochre',
-        hex = '#C27E29'
-    },
-    {
-        name = 'Peach',
-        hex = '#F1B17D'
-    },
-    {
-        name = 'Wheat',
-        hex = '#F3DCB2'
-    },
-    {
-        name = 'Tangerine',
-        hex = '#F8971E'
-    },
-    {
-        name = 'Pineapple',
-        hex = '#F8971E'
-    },
-    {
-        name = 'Lemon',
-        hex = '#F9EB1F'
-    },
-    {
-        name = 'Bright Lime',
-        hex = '#C4EB04'
-    },
-    {
-        name = 'Light Olive',
-        hex = '#A4C64B'
-    },
-    {
-        name = 'Moss Green',
-        hex = '#C6DAAD'
-    },
-    {
-        name = 'Kelly Green',
-        hex = '#379C47'
-    },
-    {
-        name = 'Asparagus',
-        hex = '#698443'
-    },
-    {
-        name = 'Dark Green',
-        hex = '#04551A'
-    },
-    {
-        name = 'Camouflage Green',
-        hex = '#677765'
-    },
-    {
-        name = 'Turquoise',
-        hex = '#1EB89E'
-    },
-    {
-        name = 'Blue in Green',
-        hex = '#068D99'
-    },
-    {
-        name = 'Powder Blue',
-        hex = '#8EB8C1'
-    },
-    {
-        name = 'Light Blue',
-        hex = '#A6C3DC'
-    },
-    {
-        name = 'Sky Blue',
-        hex = '#58B7E7'
-    },
-    {
-        name = 'Steel Blue',
-        hex = '#4470B6'
-    },
-    {
-        name = 'Slate Blue',
-        hex = '#5B6DA1'
-    },
-    {
-        name = 'Dark Blue',
-        hex = '#2B3682'
-    },
-    {
-        name = 'Pink',
-        hex = '#F9C8CA'
-    },
-    {
-        name = 'Lilac',
-        hex = '#B87AA1'
-    },
-    {
-        name = 'Plum',
-        hex = '#B34C9B'
-    },
-    {
-        name = 'Neon Violet',
-        hex = '#E61DC4'
-    },
-    {
-        name = 'Deep Purple',
-        hex = '#73138D'
-    },
-    {
-        name = 'Graphite',
-        hex = '#737373'
-    },
-    {
-        name = 'Bright Grey',
-        hex = '#C9C9C9'
-    },
+    { name = 'Burgundy',         hex = '#9D1D21' },
+    { name = 'Red',              hex = '#D62127' },
+    { name = 'Orange',           hex = '#DE5F26' },
+    { name = 'Brown',            hex = '#854F20' },
+    { name = 'Ochre',            hex = '#C27E29' },
+    { name = 'Peach',            hex = '#F1B17D' },
+    { name = 'Wheat',            hex = '#F3DCB2' },
+    { name = 'Tangerine',        hex = '#F8971E' },
+    { name = 'Pineapple',        hex = '#F8971E' },
+    { name = 'Lemon',            hex = '#F9EB1F' },
+    { name = 'Bright Lime',      hex = '#C4EB04' },
+    { name = 'Light Olive',      hex = '#A4C64B' },
+    { name = 'Moss Green',       hex = '#C6DAAD' },
+    { name = 'Kelly Green',      hex = '#379C47' },
+    { name = 'Asparagus',        hex = '#698443' },
+    { name = 'Dark Green',       hex = '#04551A' },
+    { name = 'Camouflage Green', hex = '#677765' },
+    { name = 'Turquoise',        hex = '#1EB89E' },
+    { name = 'Blue in Green',    hex = '#068D99' },
+    { name = 'Powder Blue',      hex = '#8EB8C1' },
+    { name = 'Light Blue',       hex = '#A6C3DC' },
+    { name = 'Sky Blue',         hex = '#58B7E7' },
+    { name = 'Steel Blue',       hex = '#4470B6' },
+    { name = 'Slate Blue',       hex = '#5B6DA1' },
+    { name = 'Dark Blue',        hex = '#2B3682' },
+    { name = 'Pink',             hex = '#F9C8CA' },
+    { name = 'Lilac',            hex = '#B87AA1' },
+    { name = 'Plum',             hex = '#B34C9B' },
+    { name = 'Neon Violet',      hex = '#E61DC4' },
+    { name = 'Deep Purple',      hex = '#73138D' },
+    { name = 'Graphite',         hex = '#737373' },
+    { name = 'Bright Grey',      hex = '#C9C9C9' },
 }
 
 -- colorPicker:setup(app, type)
